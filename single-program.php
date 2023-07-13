e62ffb929f30f527c52ea2214d1a23aca2ac4fc1
nicole-nam <eunjungnam@gmail.com>
2023-07-13 11:47:02 -0400

    Grabbed the related event of the program

--- single-program.php
+++ single-program.php
@@ -35,7 +35,59 @@ while(have_posts()){
         <p><?php the_content(); ?>
         </p>
     </div>
+
+    <?php 
+
+$today = date('Ymd');
+$eventPosts = new WP_Query(array(
+    'post_type' => 'event',
+    'posts_per_page' => 2,
+    'meta_key' => 'event_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+    'meta_query' => array(
+        array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value'=> $today,
+            'type' => 'numeric'
+        ),
+        array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"'
+        )
+    )
+));
+
+while($eventPosts->have_posts()){
+$eventPosts->the_post(); ?>
+
+    <div class="event-summary">
+        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month"><?php 
+        $eventDate = new DateTime(get_field('event_date'));
+        echo $eventDate-> format('M');
+        ?></span>
+            <span class=" event-summary__day"><?php echo $eventDate-> format('d');?></span>
+        </a>
+        <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a
+                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if(has_excerpt()){
+            echo get_the_excerpt();
+        }else{
+            echo wp_trim_words(get_the_content(), 18);
+        } ?><a href="<?php the_permalink(); ?>" class="nu gray">&nbsp;Learn
+                    more</a></p>
+        </div>
+    </div>
+
+    <?php }  wp_reset_postdata();
+?>
+
 </div>
+
 
 <?php }
 
