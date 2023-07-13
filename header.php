49ceeee2c2cfd6d3d249aa91c05636bce184ae6a
nicole-nam <eunjungnam@gmail.com>
2023-07-13 11:51:56 -0400

    Added navigation styling for programs

--- header.php
+++ header.php
@@ -32,7 +32,9 @@
                             <a href="<?php echo site_url('/about-us')?>">About
                                 Us</a>
                         </li>
-                        <li><a href="<?php echo site_url('/programs')?>">Programs</a></li>
+                        <li <?php if(get_post_type() == 'program') echo 'class="current-menu-item"'?>>
+                            <a href="<?php echo site_url('/programs')?>">Programs</a>
+                        </li>
                         <li
                             <?php if(get_post_type() == 'event' OR is_page() == 'past-events') echo 'class="current-menu-item"' ?>>
                             <a href="<?php echo get_post_type_archive_link('event')?>">
