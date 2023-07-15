5b24c7bb273484a91a260ad8e9adb699355e3e9f
nicole-nam <eunjungnam@gmail.com>
2023-07-14 21:47:15 -0400

    Added adjusted image size for featured images

--- single-program.php
+++ single-program.php
@@ -63,7 +63,7 @@ if($relatedProfessor->have_posts()){
 
     <li class="professor-card__list-item">
         <a class="professor-card" href="<?php the_permalink(); ?>">
-            <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+            <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
             <span class=" professor-card__name"><?php the_title(); ?></span>
         </a>
     </li>
